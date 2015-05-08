799c62968f396bc9dff7acdcea20347fc3a35ad0
dutangc <dutangc@gmail.com>
2015-05-08 10:36:59 +0200

    step 1 : remove deprecated functions : mbbefdExposure, rmbbefdC, qmbbefdC, dmbbefd2, pmbbefd2, dG.

--- R/mbbefdR-fromCppfile.R
+++ R/mbbefdR-fromCppfile.R
@@ -4,74 +4,36 @@
 
 .G<-function(x, a, b, g)
 {
-  if(missing(a)){
-    # using b, g
-    if(identical(g, 1) | identical(b, 0))
-      return(x)
-    if(identical(b, 1) & g > 1)
-      return(
-        log(1+(g-1)*x)/log(g)
-      )
-    if(identical(b*g, 1) & g>1)
-      return((1-b^x)/(1-b))
-    else
-      return(
-        log(((g-1)*b + (1-g*b)*b^x)/(1-b))/log(g*b)  
-      )
-  }else{
-    # using a nd b 
-    if(identical(a, 0) | identical(b, 1))
-      return(x)
-    if(a*(1-b)>0)
-      return( 
-        (log(a+b^x)-log(a+1))/(log(a+b)-log(a+1))
-      )
-    else
-      return((1-b^x) / (1-b))
-  }
+  stop("deprecated.")
 }
 
 #its derivative
 dG<-function(x,a,b,g)
 {
-  if(missing(a)) a<-g2a(g=g,b=b)
-  out<-( (log(b)*b^x) /(a+b^x) )/(log(a+b)-log(a+1))
-  return(out)
+  stop("deprecated.")
 }
 
 #the survival function
 .Sx<-function(x,a,b,g)
 {
-  if(missing(a)) a<-g2a(g=g,b=b)
-  out<-dG(x=x,a=a,b=b)/dG(x=0,a=a,b=b)
-  return(out)
+  stop("deprecated.")
 }
 
 #the function to compute the exposure function
-
+#could add the following line mbbefdExposure <- ecmbbefdR in mbbefdR-1stparam.R ?
 mbbefdExposure<-function(x, a, b, g)
 {
-  if(x>1||x<0) 
-    stop("Error! x should be between 0 and 1")  #check parameters coherence
-  if(b<0) 
-    stop("b should be greater or equal 0")
-  if(missing(a)){
-    if(g <= 0) stop("g has to be greater than 0")
-    .G(x, g=g,b=b)
-  }
-  else
-    .G(x=x, a=a, b=b)
+ stop("deprecated.")
 }
+
+
 
 ####################################
 #classical functions
 #distribution function
 pmbbefd2<-function(q,a,b,g)
 {
-  if(missing(a)) a<-g2a(g=g,b=b)
-  if(q>1||q<0) stop("Error! q should lie between 0 and 1")
-  out<-1-.Sx(x=q,a=a,b=b)
-  return(out)
+  stop("deprecated.")
 }
 
 #random generation function (now using the Rcpp version)
@@ -84,11 +46,7 @@ pmbbefd2<-function(q,a,b,g)
 
 dmbbefd2<-function(x,a,b,g)
 {
-  if(missing(a)) a<-g2a(g=g,b=b)
-  if(x>1||x<0) stop("Error! x should lie between 0 and 1")
-  out <-  -(((a+1)*a*log(b)*b^x)/((a+b^x)^2))*(x<1)+(x==1)*(a+1)*b/(a+b)
-  #out <- .dmbbefdC(x=x,a=a,b=b) #TODO: check why it does not work
-  return(out)
+  stop("deprecated.")
 }
 
 
--- tests/testthat/testsDistr.R
+++ tests/testthat/testsDistr.R
@@ -5,21 +5,20 @@ library(mbbefd)
 context("Check some values with Mahler")
 
 test_that("some values", {
-  expect_equal(round(mbbefdExposure(x=1/2,a = .2,b = .04),2), 0.68)
-  expect_equal(1/dG(x=0,a=.2,b=.04),.6)
+  expect_equal(round(ecmbbefd(x=1/2,a = .2,b = .04),2), 0.68)
   expect_equal(round(dmbbefd(x = .1, a=.2, b=.04),2), .65)
   expect_equal(round(1-pmbbefd(q= .6, a=.2, b=.04),4),.5043)  
 })
 
 
 test_that("mbbefdExposure", {
-  expect_equal(mbbefdExposure(0.5, a=1, b=1), 0.5)  
-  expect_equal(mbbefdExposure(0.5, a=0, b=1), 0.5) 
-  expect_equal(mbbefdExposure(0.5, a=0, b=0), 0.5)  
-  expect_equal(mbbefdExposure(0.5, a=1, b=0), 1)  
+  expect_equal(ecmbbefd(0.5, a=1, b=1), 0.5)  
+  expect_equal(ecmbbefd(0.5, a=0, b=1), 0.5) 
+  expect_equal(ecmbbefd(0.5, a=0, b=0), NaN)  
+  expect_equal(ecmbbefd(0.5, a=1, b=0), NaN)  
   
-  expect_equal(mbbefdExposure(0.5, g=1, b=1), 0.5)  
-  expect_equal(mbbefdExposure(0.5, g=1, b=0), 0.5)
+  expect_equal(ecMBBEFD(0.5, g=1, b=1), 0.5)  
+  expect_equal(ecMBBEFD(0.5, g=1, b=0), 0.5)
   
 })
 

567a7fa801c2b403dc77e40b299105e252c384d0
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2015-05-17 22:32:34 +0200

    add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -14,6 +14,21 @@
 # 	)
 # }
 
+#adding a startup message
+
+.onLoad <- function(libname, pkgname) {
+  
+  desc  <- packageDescription(pkgname, libname)
+  
+  packageStartupMessage('This is package: ',desc$Package, '\n',
+    'Version:  ', desc$Version, '\n', 
+    'Date:     ', desc$Date, '\n'
+  )
+  message("Please note <d,q,p,r>mbbefd functions have been split into  <d,q,p,r>mbbefd and  <d,q,p,r>MBBEFD")
+  message("depending the group of parameters being used. Update your code whether necessary")
+}
+
+
 # for unloading dynamic libraries
 
 .onUnload <- function (libpath) {

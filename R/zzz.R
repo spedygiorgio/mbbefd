860c62e50d4e18ab2e1345f8ee9f721d6160848d
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2015-05-25 23:34:41 +0200

    Improved startup

--- R/zzz.R
+++ R/zzz.R
@@ -5,12 +5,17 @@
 
 #adding a startup message
 
-.onLoad <- function(libname, pkgname) {
-packageStartupMessage('This is mbbefd package. ','\n',
+onAttach <- function(libname, pkgname) {
+  desc  <- packageDescription(pkgname, libname)
+  packageStartupMessage('Package:  ', desc$Package, '\n',
+                        'Version:  ', desc$Version, '\n', 
+                        'Date:     ', desc$Date, '\n',
+                        'BugReport: ', desc$BugReports, '\n',
                         "Please note <d,q,p,r>mbbefd functions have been split into  <d,q,p,r>mbbefd and  <d,q,p,r>MBBEFD",'\n',
-                        "depending respectively whether (a,b) or (g,b) are used. Update your code whether necessary"
-  )
+                        "depending respectively whether (a,b) or (g,b) are used. Update your code whether necessary")
 }
+
+
 
 
 # for unloading dynamic libraries

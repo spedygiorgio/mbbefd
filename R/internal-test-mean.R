aacfcf65b2e96baf9e2497550e15d56e87f73e45
dutangc <dutangc@gmail.com>
2015-06-01 14:23:16 +0200

    update test file for expectation.

--- R/internal-test-mean.R
+++ R/internal-test-mean.R
@@ -4,6 +4,6 @@ tmean1 <- function(doifun, ...)
   integrate(function(x) x*doifun(x, ...), 0, 1)$value + 1*doifun(x=1, ...)
 tmean2 <- function(poifun, ...)
   integrate(function(x) 1-poifun(x, ...), 0, 1)$value
-tmean3 <- function(x, ecoifun, eps=1e-9, ...)
+tmean3 <- function(ecoifun, eps=1e-9, ...)
   eps/ecoifun(eps, ...)
 

b703643ea9d4ef2d64608b4bde7a861e466ac14b
dutangc <dutangc@gmail.com>
2016-07-07 13:37:26 +0200

    update test file for numerical illustration on lossalae.

--- tests/test-lossalae.R
+++ tests/test-lossalae.R
@@ -11,12 +11,20 @@ flist <- lapply(dlist, function(d) {print(d);
   fitDR(x, d, method="mle")})
 names(flist) <- dlist
 
+gofstat(flist)
 
-cdfcomp(flist, do.points=FALSE, leg=dlist, xlogscale = TRUE)
+cdfcomp(flist, do.points=FALSE, leg=dlist, addlegend = FALSE, lwd=1.5)
+legend("topleft", col=c("red", "green", "blue"), leg=dlist, lty=1:3, lwd=2, bty="n")
+
+
+cdfcomp(flist, do.points=FALSE, leg=dlist, xlogscale = TRUE, addlegend = FALSE, lwd=1.5)
+legend("topleft", col=c("red", "green", "blue"), leg=dlist, lty=1:3, lwd=2, bty="n")
+
+eccomp(flist, leg=dlist, do.points = FALSE, addlegend = FALSE, lwd=1.5)
+legend("topleft", col=c("red", "green", "blue"), leg=dlist, lty=1:3, lwd=2, bty="n")
 
 ppcomp(flist, leg=dlist, fitpch=".", addlegend = FALSE)
 legend("bottomright", fill=c("red", "green", "blue", "cyan", "purple"), leg=dlist)
 
-eccomp(flist, leg=dlist, do.points = FALSE)
 
 qqcomp(flist, leg=dlist, use.ppoints=TRUE)

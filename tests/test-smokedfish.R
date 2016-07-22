0b4416790dba3bf5cc2ed81eb871c34673130901
dutangc <dutangc@gmail.com>
2016-07-22 08:38:45 +0200

    update smokedfish dataset file.

--- tests/test-smokedfish.R
+++ tests/test-smokedfish.R
@@ -6,17 +6,26 @@ data(smokedfish)
 listeria <- apply(smokedfish, 1, mean, na.rm=TRUE) / 100
 #purety rate
 set.seed(1234)
-x <- pmin(pmax(jitter(1-listeria, amount=.1), 0.01), 1)
+x <- pmin(pmax(jitter(1-listeria, amount=.2), 0.01), 1)
 
-mledist(x[x!=1], "stpareto", start=list(a=2), optim.method="Nelder", control=list(trace=1, REPORT=1), lower=.01)
+#mledist(x[x!=1], "stpareto", start=list(a=2), optim.method="Nelder", control=list(trace=1, REPORT=1), lower=.01)
 
-fitDR(x, "oistpareto", control=list(trace=1, REPORT=1), start=list(a=0.01), optim.method="Nelder")
-fitDR(x, "oibeta", control=list(trace=1, REPORT=1))
-fitDR(x, "oigbeta", control=list(trace=1, REPORT=1))
-
-dlist <- c("oistpareto", "oibeta", "oigbeta")
-flist <- lapply(dlist, function(d) {print(d);
-  fitDR(x, d, method="mle")})
-names(flist) <- dlist
+#, control=list(trace=1, REPORT=1)
+flist <- list(
+  fitDR(x, "oistpareto", start=list(a=0.01), optim.method="Nelder"),
+  fitDR(x, "oibeta"), fitDR(x, "oigbeta"))
+names(flist) <- dlist <- c("oistpareto", "oibeta", "oigbeta")
 
 gof1 <- gofstat(flist)
+
+mm <- rbind(KS = gof1$ks, CvM = gof1$cvm, AD = gof1$ad, AIC = gof1$aic, BIC = gof1$bic)
+rownames(mm) <- c("Kolmogorov-Smirnov statistic", "Cramer-von Mises statistic", 
+                  "Anderson-Darling statistic", "Aikake's Information Criterion", "Bayesian Information Criterion")
+
+
+cdfcomp(flist, do.points=FALSE, leg=dlist, addlegend = FALSE, lwd=1.5)
+legend("topleft", col=c("red", "green", "blue"), leg=dlist, lty=1:3, lwd=2, bty="n")
+
+#log scale
+cdfcomp(flist, do.points=FALSE, leg=dlist, xlogscale = TRUE, addlegend = FALSE, lwd=1.5)
+legend("topleft", col=c("red", "green", "blue"), leg=dlist, lty=1:3, lwd=2, bty="n")

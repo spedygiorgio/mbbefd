ac83f4816dbf5d596d5ae53decf1cfc3c18e0d1f
Giorgio Alfredo Spedicato <spedygiorgio@gmail.com>
2017-02-20 22:58:43 +0100

    native routines registering

--- src/init.c
+++ src/init.c
@@ -4,7 +4,7 @@
 #include <R_ext/Rdynload.h>
 
 /* FIXME: 
-Check these declarations against the C/Fortran source code.
+   Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
@@ -13,14 +13,14 @@ extern SEXP mbbefd_rmbbefdC(SEXP, SEXP, SEXP);
 extern SEXP mbbefd_rMBBEFDC(SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"mbbefd_g2a",      (DL_FUNC) &mbbefd_g2a,      2},
-  {"mbbefd_rmbbefdC", (DL_FUNC) &mbbefd_rmbbefdC, 3},
-  {"mbbefd_rMBBEFDC", (DL_FUNC) &mbbefd_rMBBEFDC, 3},
-  {NULL, NULL, 0}
+    {"mbbefd_g2a",      (DL_FUNC) &mbbefd_g2a,      2},
+    {"mbbefd_rmbbefdC", (DL_FUNC) &mbbefd_rmbbefdC, 3},
+    {"mbbefd_rMBBEFDC", (DL_FUNC) &mbbefd_rMBBEFDC, 3},
+    {NULL, NULL, 0}
 };
 
 void R_init_mbbefd(DllInfo *dll)
 {
-  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(dll, FALSE);
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }

2db35fe3fbd01ccf9f8fca31e33bfd5ec5159887
Christophe Dutang <dutangc@gmail.com>
2023-08-22 17:00:20 +0200

    wide update

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // g2a
 double g2a(double g, double b);
 RcppExport SEXP _mbbefd_g2a(SEXP gSEXP, SEXP bSEXP) {
